ba20a21bdb7e3c08b1d512011220a897d73dc0eb
Bolarinwa Owuogba <bhorlarinwah@gmail.com>
2022-05-12 09:26:33 +0100

    ch: make existing tests pass. ch: add new test cases for modifySneakerPrice method.

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -18,16 +18,14 @@ contract NFTMarketplace is ReentrancyGuard {
         bool sold;
     }
 
-    
     mapping(uint256 => Sneaker) public sneakers;
-    
 
     function listSneaker(
         IERC721 _nft,
         uint256 _tokenId,
         uint256 _price
-    ) external nonReentrant{
-        require(_price > 0, "Price should be above zero");
+    ) external nonReentrant {
+        require(_price > 0, "NFTMarketplace: Price should be above zero");
         sneakerCount++;
         _nft.transferFrom(msg.sender, address(this), _tokenId);
         sneakers[sneakerCount] = Sneaker(
@@ -38,27 +36,37 @@ contract NFTMarketplace is ReentrancyGuard {
             false
         );
     }
-   
+
     function buySneaker(uint256 _tokenId) external payable nonReentrant {
         Sneaker storage sneaker = sneakers[_tokenId];
-        require(_tokenId > 0 && _tokenId <= sneakerCount, "sneaker doesn't exist");
-        require(msg.value >= sneaker.price, "not enough balance");
-        require(!sneaker.sold, "sneaker already sold");
+        require(
+            _tokenId > 0 && _tokenId <= sneakerCount,
+            "NFTMarketplace: sneaker doesn't exist"
+        );
+        require(
+            msg.value >= sneaker.price,
+            "NFTMarketplace: not enough balance"
+        );
+        require(!sneaker.sold, "NFTMarketplace: sneaker already sold");
         sneaker.seller.transfer(sneaker.price);
-        sneaker.sold = true;        
+        sneaker.sold = true;
         sneaker.nft.transferFrom(address(this), msg.sender, sneaker.tokenId);
     }
 
-    function getSneakers(uint256 _tokenId) public view returns (Sneaker memory) {
+    function getSneaker(uint256 _tokenId) public view returns (Sneaker memory) {
         return sneakers[_tokenId];
     }
 
-    function modifySneakerPrice(uint _sneakerPrice, uint _tokenId) public payable {
-        require(sneakers[_tokenId].seller == msg.sender, "Can't perform transaction");
+    function modifySneakerPrice(uint256 _sneakerPrice, uint256 _tokenId)
+        public
+        payable
+    {
+        require(
+            sneakers[_tokenId].seller == msg.sender,
+            "NFTMarketplace: Can't perform transaction"
+        );
         sneakers[_tokenId].price = _sneakerPrice;
     }
-    
-
 
     function getSneakerCount() public view returns (uint256) {
         return sneakerCount;
